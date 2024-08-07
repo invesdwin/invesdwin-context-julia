eeb306ff2eb929c8afbab0add9f9d52c053adf3a
subes <gsubes@gmail.com>
2024-08-07 23:41:33 +0200

    comment

--- invesdwin-context-julia-parent/invesdwin-context-julia-runtime-libjuliaclj/src/test/java/de/invesdwin/context/julia/runtime/libjuliaclj/LibjuliacljScriptTaskRunnerJuliaTest.java
+++ invesdwin-context-julia-parent/invesdwin-context-julia-runtime-libjuliaclj/src/test/java/de/invesdwin/context/julia/runtime/libjuliaclj/LibjuliacljScriptTaskRunnerJuliaTest.java
@@ -5,11 +5,12 @@ import javax.annotation.concurrent.NotThreadSafe;
 import org.junit.jupiter.api.Test;
 
 import de.invesdwin.context.julia.runtime.contract.InputsAndResultsTests;
-import de.invesdwin.context.julia.runtime.contract.callback.SimpleCallbackTest;
 import de.invesdwin.context.julia.runtime.contract.callback.ParametersAndReturnsTests;
+import de.invesdwin.context.julia.runtime.contract.callback.SimpleCallbackTest;
 import de.invesdwin.context.test.ATest;
 import jakarta.inject.Inject;
 
+// LD_PRELOAD=/usr/lib/jvm/default-java/lib/libjsig.so
 @NotThreadSafe
 public class LibjuliacljScriptTaskRunnerJuliaTest extends ATest {
 
